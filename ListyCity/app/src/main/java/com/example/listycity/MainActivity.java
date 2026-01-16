8ca6578b56473980de11fa9e0d82efff8de5a388
Summayah Javed <summayah.joyia@gmail.com>
2026-01-16 10:37:54 -0700

    Lab participation activity- works

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.listycity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,21 +20,30 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    EditText cityInput;
+    Button addButton;
+    Button deleteButton;
+    int selectedPosition = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //do we have to remove this?
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //links activity java to the xml file
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
         cityList = findViewById(R.id.city_list);
+        cityInput = findViewById(R.id.entercity);
+        addButton = findViewById(R.id.add_button);
+        deleteButton = findViewById(R.id.delete_button);
+
         String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney","Berlin","Vienna","Tokyo","Beijing","Osaka","New Delhi"};
 
         dataList = new ArrayList<>();
@@ -36,6 +51,33 @@ public class MainActivity extends AppCompatActivity {
 
         cityAdapter = new ArrayAdapter<>(this,R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
+        //saving position of what city is tapped(clicked)
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedPosition = position;
+            }
+        });
+        //https://stackoverflow.com/questions/3766900/how-to-take-input-from-user-in-android
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String cityName = cityInput.getText().toString();
+                dataList.add(cityName);
+                cityAdapter.notifyDataSetChanged();
+                cityInput.setText("");
+            }
+        });
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedPosition != -1) {
+                    dataList.remove(selectedPosition);
+                    cityAdapter.notifyDataSetChanged();
+                    selectedPosition = -1;
+                }
+            }
+        });
 
     }
 }
